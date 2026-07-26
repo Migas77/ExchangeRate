911fbe4b558a97ec1dabec260e2297d1628f312f
Migas77 <miguel.belchior@ua.pt>
2026-07-26 10:05:28 +0100

    test(rates-controller): testing controller with mock rates service
    Test Cases:
    - valid request with only query parameter 'from'
    - valid request with query parameter 'from' and 'to' specified
    - invalid request with invalid query parameters (using ParameterizedTest)
    - test to check if GLobalExceptionHandler Bean is configured
    - unexpected exception handled and returned as a ProblemDetail with status 500 (by GlobalExceptionHandler)

---
+++ src/test/java/com/miguelbf/exchangerateapi/utilities/ArgumentCombinations.java
@@ -0,0 +1,30 @@
+package com.miguelbf.exchangerateapi.utilities;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ArgumentCombinations {
+
+	public static Stream<Arguments> allCombinations(List<?>... lists) {
+		return Arrays.stream(lists)
+			.reduce(
+				Stream.of(Collections.emptyList()),
+				(combinations, list) -> combinations
+					.flatMap(combo -> list.stream()
+						.map(value -> {
+							List<Object> next = new ArrayList<>(combo);
+							next.add(value);
+							return next;
+						})
+					),
+				Stream::concat
+			)
+			.map(combo -> Arguments.of(combo.toArray()));
+	}
+
+}
